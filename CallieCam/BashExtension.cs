533209a29bba6ee1d8848481e09959d479f3b529
Riley Nielsen <thatr2d2cat@gmail.com>
2021-09-24 21:19:43 -0500

    Add BashExtension and test GetSensorData method.

---
+++ CallieCam/BashExtension.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace CallieCam
+{
+    public static class BashExtension
+    {
+        public static string Bash(this string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            return result;
+        }
+    }
+}
